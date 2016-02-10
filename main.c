e198473688644f1f3b1a7334a426f8551872c04b
Damien Leroy <damien7leroy@gmail.com>
2016-02-10 16:21:17 +0100

    get_buf and free_buf

--- main.c
+++ main.c
@@ -10,17 +10,51 @@
 #include <pthread.h>
 #include <netdb.h>
 
-#define BCAST_ADDR "192.168.10.255"
+#define BCAST_ADDR "192.168.10.255"	// ou #define BCAST_ADDR (192<<24+168<<16+3<<8+255) puis htons(BCAST_ADDR)
 #define BCAST_PORT 1234
 
+
+typedef enum msg_type
+{
+    MT_INVAL = 0,
+    MT_HELLO = 1,
+    MT_MSG   = 2,
+    MT_NICK  = 3,
+    MT_COLOR = 4,
+    MT_MAX,
+} msg_type_t;
+
+#define BUF_SIZE 1024
+typedef struct msg
+{
+    msg_type_t  type;
+    uint16_t    len;
+    char        buf[BUF_SIZE];
+} msg_t;
+#define MSG_SIZE sizeof(msg_t)
+
+
+
+
 /***** GLOBAL VARIABLES *****/
+
 int bcast_sock = -1;
 struct sockaddr bcast_addr;
 
+
+
+
 /***** PROTOTYPE DE FONCTIONS *****/
+
 int init_bcast(struct sockaddr *bcast_addr);
+msg_t *get_buf(msg_type_t type);
+void free_buf(msg_t *buf);
+
+
+
 
 /***** MAIN *****/
+
 int main (int argc, char **argv)
 {
 	printf("Hello world !\n");
@@ -30,7 +64,11 @@ int main (int argc, char **argv)
 	return 0;
 }
 
+
+
+
 /***** FONCTIONS *****/
+
 int init_bcast(struct sockaddr *bcast_addr)
 {
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -44,5 +82,35 @@ int init_bcast(struct sockaddr *bcast_addr)
 		printf("Erreur init_bcast\n");
 	
 	
-	return sock;
+	return sockfd;
+}
+
+msg_t *get_buf(msg_type_t type)
+{
+    msg_t *msg = NULL;
+
+    char* buffer = malloc((BUF_SIZE + 1) * sizeof(char));
+    int byteRead;
+    int i = 0;
+	while((byteRead = recv(bcast_sock, buffer, BUF_SIZE, 0)) > 0)
+	{
+		if(byteRead <= 0)
+		    break;
+		else {
+		    msg->buf[i] = *buffer;
+		    //printf("%s", buffer);
+		}
+		i++;
+	}
+	msg->buf[i+1] = '\0';
+	
+	msg->type = type;
+	msg->len = i+1;
+
+    return msg;
+}
+
+void free_buf(msg_t *buf)
+{
+    free(buf);
 }
