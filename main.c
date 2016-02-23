e293558c2286640f76a145288fbb350fb9821560
Damien Leroy <damien7leroy@gmail.com>
2016-02-23 23:18:00 +0100

    Gestion des types de messages et des commandes

--- main.c
+++ main.c
@@ -10,10 +10,12 @@
 #include <pthread.h>
 #include <netdb.h>
 
+#include "sys/queue.h"
+
 //#define BCAST_ADDR "192.168.10.255"	// ou #define BCAST_ADDR (192<<24+168<<16+3<<8+255) puis htons(BCAST_ADDR)
 //#define BCAST_PORT 1234
 
-#define BCAST_ADDR "192.168.1.255"	// ou #define BCAST_ADDR (192<<24+168<<16+3<<8+255) puis htons(BCAST_ADDR)
+#define BCAST_ADDR "127.0.0.1"	// ou #define BCAST_ADDR (192<<24+168<<16+3<<8+255) puis htons(BCAST_ADDR)
 #define BCAST_PORT 5000
 
 
@@ -37,21 +39,22 @@ typedef struct msg
 #define MSG_SIZE sizeof(msg_t)
 
 
-#define NB_USER 100
+#define NB_USER 5
 #define NICK_DEFAULT "Guest"
-#define COLOR_DEFAULT (colors[0].name)
+#define COLOR_DEFAULT 1
 #define NICK_LEN 256
 #define NODE_INFO_LEN 256
 typedef struct user
 {
+	TAILQ_ENTRY(user)       lh;
     struct sockaddr_in      sa;
     char                    nick[NICK_LEN];
     char                    node_info[NODE_INFO_LEN];
-    const char             *color;
+    int                     color;
+    int                     last_msg;
 } user_t;
 
-user_t users[NB_USER];
-int user_online = 0;
+TAILQ_HEAD(mylist, user) users_list;
 
 
 
@@ -78,6 +81,8 @@ void *receive_thread(void *arg);
 void process_received_msg(user_t *user, msg_t *msg);
 void send_msg(msg_t *data, size_t data_len);
 void enter_loop(void);
+void *hello_thread(void *args);
+void free_user();
 
 
 
@@ -86,16 +91,23 @@ void enter_loop(void);
 
 int main (int argc, char **argv)
 {
-	printf("Bienvenue, vous pouvez discutez avec les personnes connectées sur le réseau !\n");
+    printf("\x1B[0mBienvenue, vous pouvez discutez avec les personnes connectées sur le réseau !\n");
 	
 	pthread_t receive_th;
+	pthread_t hello_th;
 	int rc;
+
+	TAILQ_INIT(&users_list);
 
 	bcast_sock = init_bcast(&bcast_addr);
 
     rc = pthread_create(&receive_th, NULL, receive_thread, NULL);
     if (rc < 0)
     	printf("Cannot create receive thread\n");
+
+    rc = pthread_create(&hello_th, NULL, hello_thread, NULL);
+    if (rc < 0)
+    	printf("Cannot create hello thread\n");
 
     enter_loop();
 
@@ -157,10 +169,9 @@ user_t *add_user(struct sockaddr_in *sa)
 		user->sa = *sa;
 		strncpy(user->nick, NICK_DEFAULT, NICK_LEN);
 		get_node_info(user, sa);
-		//user.color = COLOR_DEFAULT;
+		user->color = COLOR_DEFAULT;
 	
-		users[user_online] = *user;
-		user_online++;
+		TAILQ_INSERT_TAIL(&users_list, user, lh);
 	}
 	else
 		printf("ERREUR : Allocation utilisateur échouée\n");
@@ -172,51 +183,38 @@ void get_node_info(user_t *user, struct sockaddr_in *sa)
 {
 	int port = sa->sin_port;
 	char *ip = inet_ntoa(sa->sin_addr);
-	sprintf(user->node_info, "%d", port);
-	strcat(user->node_info, ":");
-	strcat(user->node_info, ip);
+	sprintf(user->node_info, "%s:%d", ip, port);
 }
 
 void del_user(user_t *user)
 {
-	int i;
-	int supprimer = 0;
-	for(i=0 ; i < user_online-1 ; i++)
-	{
-		if (supprimer == 0 && memcmp(&(&users[i])->sa, &user->sa, sizeof(struct sockaddr_in)) == 0)
-			supprimer = 1;
-		if (supprimer == 1)		
-			users[i] = users[i+1];
-	}
-	user_t empty;
-	users[user_online-1] = empty;
-	user_online--;
+    printf("%s disconnected\n", user->nick);
+	TAILQ_REMOVE(&users_list, user, lh);
 }
 
 user_t *lookup_user(struct sockaddr_in *sa)
 {
-	user_t *user = NULL;
-	int i;
-    for(i=0 ; i < user_online-1 ; i++)
-	{
-		if (memcmp(&(&users[i])->sa, &sa, sizeof(struct sockaddr_in)) == 0)
-		{
-			user = &(user[i]);
-			return user;
-		}
-	}
-    return user;
+    user_t *user = NULL;
+
+    TAILQ_FOREACH(user, &users_list, lh)
+    {
+        if (memcmp(&(user->sa), sa, sizeof(struct sockaddr_in)) == 0)
+        	return user;
+    }
+    return NULL;
 }
 
 void show_users()
 {
-	int i;
-    for(i=0 ; i < user_online-1 ; i++)
-	{
-		printf("User %s\n", users[i].nick);
-		printf("\tUsing color %s\n", users[i].color);
-		printf("\tConnected with %s\n", users[i].node_info);
-	}
+	user_t *user = NULL;
+
+    TAILQ_FOREACH(user, &users_list, lh)
+    {
+        printf("User %s\n", user->nick);
+		printf("\tUsing color %d\n", user->color);
+		printf("\tConnected with %s\n", user->node_info);
+        printf("\tLast message %d seconds ago\n", (int)time(NULL)-user->last_msg);
+    }
 }
 
 void *receive_thread(void *arg)
@@ -239,35 +237,51 @@ void *receive_thread(void *arg)
 
 			process_received_msg(sender, (msg_t*) data);
 		}
+        free(data);
+        free(msg);
     }
     return NULL;
 }
 
 void process_received_msg(user_t *user, msg_t *msg)
 {
+    user_t *userq = NULL;
+    TAILQ_FOREACH(userq, &users_list, lh)
+    {
+        if (memcmp(userq, user, sizeof(user_t)) == 0)
+            user->last_msg = (int)time(NULL);
+    }
     switch(msg->type)
     {
         case MT_HELLO:
             {
-                printf("%d\n", msg->type);
+                // Nothing more than default
             }
             break;
 
         case MT_NICK:
             {
-                printf("%d\n", msg->type);
+                TAILQ_FOREACH(userq, &users_list, lh)
+                {
+                    if (memcmp(userq, user, sizeof(user_t)) == 0)
+                        strncpy(user->nick, msg->buf, NICK_LEN);
+                }
             }
             break;
 
         case MT_MSG:
             {
-                printf("%s : %d : %s", inet_ntoa(user->sa.sin_addr), msg->type, msg->buf);
+                printf("\x1B[%dm%s : %s\x1B[0m\n", user->color, user->nick, msg->buf);
             }
             break;
 
         case MT_COLOR:
             {
-                printf("%d\n", msg->type);
+                TAILQ_FOREACH(userq, &users_list, lh)
+                {
+                    if (memcmp(userq, user, sizeof(user_t)) == 0)
+                        userq->color = atoi(msg->buf);
+                }
             }
             break;
 
@@ -290,11 +304,86 @@ void enter_loop(void)
         char str[BUF_SIZE];
 
         fgets(str, BUF_SIZE, stdin);
+        int i;
+        for (i=0 ; i<BUF_SIZE ; i++)
+        {
+            if (str[i] == '\n')
+                str[i] = '\0';
+        }
+        if (str[0] != '/')
+        {
+            msg_t * msg = get_buf(MT_MSG);
+    		msg->len = BUF_SIZE;
+    		strncpy(msg->buf, str, BUF_SIZE);
+            send_msg(msg, MSG_SIZE);
+            free_buf(msg);
+        }
+        else
+        {
+            switch(str[1])
+            {
+                case 'n':
+                    {
+                        msg_t * msg = get_buf(MT_NICK);
+                        msg->len = BUF_SIZE;
+                        memmove(str, str+3, strlen(str));
+                        strncpy(msg->buf, str, BUF_SIZE);
+                        send_msg(msg, MSG_SIZE);
+                        free_buf(msg);
+                    }
+                    break;
 
-        msg_t * msg = get_buf(MT_MSG);
+                case 'c':
+                    {
+                        msg_t * msg = get_buf(MT_COLOR);
+                        msg->len = BUF_SIZE;
+                        memmove(str, str+3, strlen(str));
+                        strncpy(msg->buf, str, BUF_SIZE);
+                        send_msg(msg, MSG_SIZE);
+                        free_buf(msg);
+                    }
+                    break;
+
+                case 's':
+                    {
+                        show_users();
+                    }
+                    break;
+
+                default:
+                    printf("Commande inconnue\n");
+                    printf("Commandes disponibles : \n");
+                    printf("/n <pseudonyme> : change le pseudonyme de l'utilisateur\n");
+                    printf("/c <couleur> : change la couleur de l'utilisateur\n");
+                    printf("/s : show users\n");
+                    break;
+            }
+        }
+    }
+}
+
+void *hello_thread(void *args)
+{
+    while (1)
+    {
+        msg_t * msg = get_buf(MT_HELLO);
 		msg->len = BUF_SIZE;
-		strncpy(msg->buf, str, BUF_SIZE);
+		strncpy(msg->buf, "hello_thread", BUF_SIZE);
         send_msg(msg, MSG_SIZE);
         free_buf(msg);
+        free_user();
+        sleep(5);
+    }
+
+    return NULL;
+}
+
+void free_user()
+{
+    user_t *user = NULL;
+    TAILQ_FOREACH(user, &users_list, lh)
+    {
+        if ((int)time(NULL)-user->last_msg > 60)
+            del_user(user);
     }
 }
